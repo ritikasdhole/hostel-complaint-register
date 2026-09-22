3abe3b60b00d3f378086c17d927064a306997a03
ritikasdhole <ritikasdhole@gmail.com>
2026-09-23 00:26:14 +0530

    test: ensure failure demo exits cleanly

--- test/app.test.js
+++ test/app.test.js
@@ -40,9 +40,10 @@ test("valid complaint can be submitted", async () => {
     redirect: "manual",
   });
 
-  assert.equal(response.status, 201);
-
   server.close();
+
+  assert.equal(response.status, 201);
+  
 });
 
 test("invalid complaint is rejected", async () => {
